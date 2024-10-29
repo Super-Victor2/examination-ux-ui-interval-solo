b6407f37bd1294dfe11ef4ec64ecdb78a325e88e
Victor <ninjavigge@gmail.com>
2024-10-29 12:31:53 +0100

    animationer klara

--- time-app/src/components/homeComp/homeComp.jsx
+++ time-app/src/components/homeComp/homeComp.jsx
@@ -1,11 +1,18 @@
 import './homeComp.css'
 import homeImg from '../../assets/Frame 2.png'
 import { Link } from 'react-router-dom';
+import { motion } from "framer-motion";
 
 function homeComp() {
     return (
         <>
-            <Link to='/setTimePage'><img className='home-logo' src={homeImg} alt="img" /> </Link>
+            <motion.div
+                initial={{ scale: 0}}
+                animate={{ scale: 1}}
+                whileHover={{ scale: 1.2}}
+            >
+                <Link to='/setTimePage'><img className='home-logo' src={homeImg} alt="img" /> </Link>
+            </motion.div>
         </>
     )
 }
